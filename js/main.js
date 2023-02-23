a61768129b22f3529ddf454b3a499d9e1c2bd6e9
AndreyFufligin <113243927+AndreyFufligin@users.noreply.github.com>
2023-02-23 12:05:12 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(function() {
+    
+  	$(".menu a, .go-top").on("click", function(e) {
+  	    //отменяем стандартную обработку нажатия по ссылке
+  	    e.preventDefault();
+  	    //забираем идентификатор блока с атрибута href
+  	    var id = $(this).attr('href'),
+  	    //узнаем высоту от начала страницы до блока на который ссылается якорь
+  	    top = $(id).offset().top;
+  	    //анимируем переход на расстояние - top за 1500 мс
+  	    $('body,html').animate({
+  	        scrollTop: top
+  	    }, 1500);
+  	});
+
+
+    //slider-slick
+    $('.slider-blog__inner').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+          responsive: [{
+                      breakpoint: 769,
+                      settings: {
+                        arrows: false,
+                        dots: true
+                      }
+                  }]
+    });
+
+
+    //burger-menu
+    $('.menu__btn, .menu a').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+    //mixitup
+    var mixer = mixitup('.portfolio__content');
+});
